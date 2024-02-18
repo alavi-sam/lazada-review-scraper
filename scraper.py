414dda93f95002693a21e3acb58ced6f7ff12d34
amirmahdi-alavi <sam.alavi@hotmail.com>
2024-02-17 22:59:40 -0500

    get total pages

--- scraper.py
+++ scraper.py
@@ -7,6 +7,11 @@ def extract_product_id(url):
     return base_part.split('-')[-2][1:]
 
 
+def get_total_page_number(product_id):
+    request_url = f'https://my.lazada.co.id/pdp/review/getReviewList?itemId={product_id}&pageSize=5&filter=0&sort=0&pageNo=1'
+    response = requests.get(request_url).json()
+    return response['model']['paging']['totalPages']
+
 def get_reviews(page, product_id):
     request_url = f'https://my.lazada.co.id/pdp/review/getReviewList?itemId={product_id}&pageSize=5&filter=0&sort=0&pageNo={page}'
     response = requests.get(request_url)
