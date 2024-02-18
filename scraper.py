11038db22cc4c3f577a5f68c692334f5de2ca663
amirmahdi-alavi <sam.alavi@hotmail.com>
2024-02-17 22:56:48 -0500

    extract product id from url

---
+++ scraper.py
@@ -0,0 +1,13 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def extract_product_id(url):
+    base_part = url.split('.html')[0]
+    return base_part.split('-')[-2][1:]
+
+
+# def get_reviews(page, product_id):
+#     request_url = f'https://my.lazada.co.id/pdp/review/getReviewList?itemId={product_id}&pageSize=5&filter=0&sort=0&pageNo={page}'
+#     response = requests.get(request_url)
+#     return response.json()
